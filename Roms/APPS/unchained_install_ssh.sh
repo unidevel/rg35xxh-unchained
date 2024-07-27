95545c871d9d38a1b2fccde31bbc35b39aee03f5
unidevel <unidevel@gmail.com>
2024-07-27 13:56:08 +0800

    remove non-exist game user and updating time script

--- Roms/APPS/unchained_install_ssh.sh
+++ Roms/APPS/unchained_install_ssh.sh
@@ -17,9 +17,9 @@ cp $progdir/UNCHAINED/etc/ssh/sshd_config /etc/ssh/sshd_config
 
 addgroup --system sshusers >> $log 2>&1
 adduser root sshusers >> $log 2>&1
-adduser game sshusers >> $log 2>&1
+# adduser game sshusers >> $log 2>&1
 
 service ssh restart >> $log 2>&1
 
 # Update Time
-date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+# date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
